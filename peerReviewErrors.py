b0c10ba6e409eee2b7e5497c1efc3c63ce11d030
jtackwell <jtackwell@gmail.com>
2022-05-27 23:49:36 -0400

    Updated with peer reviewed comments.

--- peerReviewErrors.py
+++ peerReviewErrors.py
@@ -1,14 +1,15 @@
 # This is a header for the application
 # You should read this header and insert your name and your date below as part of the peer review
 # This is a typical part of any program
-# Author: <author>
-# Creation Date: <date>
+# Author: Jonathan Tackwell
+# Creation Date: 5/27/2022
 # Below is a simple program with 10 issues (some are syntax errors and some are logic errors.  You need to identify the issues and correct them.
 
 import random
 import time
 
 def displayIntro():
+	
 	print('''You are in a land full of dragons. In front of you,
 	you see two caves. In one cave, the dragon is friendly
 	and will share his treasure with you. The other dragon
@@ -16,14 +17,15 @@ def displayIntro():
 	print()
 
 def chooseCave():
-    cave = ''
-	while cave != '1' and cave != '2':
-		print('Which cave will you go into? (1 or 2)')
-		cave = input()
+    #used spaces instead of tabs in the line below
+	cave = ''
+	while cave != '1' and cave != '2': #this while statement is unecessary, delete line for simplicity.
+		print('Which cave will you go into? (1 or 2)') #remove tabs
+		cave = input()#remove tabs
 
-	return caves
+	return caves #delete to simplifiy 
 
-def checkCave(chosenCave):
+def checkCave(chosenCave):#I would delete this line entirely to simplfy the code
 	print('You approach the cave...')
 	#sleep for 2 seconds
 	time.sleep(2)
@@ -36,17 +38,19 @@ def checkCave(chosenCave):
 	time.sleep(2)
 	friendlyCave = random.randint(1, 2)
 
-	if chosenCave == str(friendlyCave):
+	if chosenCave == str(friendlyCave): #change "chosenCave" to "cave" to complete the comparison attempted here
 		print('Gives you his treasure!')
 	else:
-		print 'Gobbles you down in one bite!'
+		print 'Gobbles you down in one bite!'#syntax error line should be "print('Gobbles you down in one bite!')"
 
 playAgain = 'yes'
-while playAgain = 'yes' or playAgain = 'y':
+ 
+while playAgain = 'yes' or playAgain = 'y': # should be '==' not '='
 	displayIntro()
-	caveNumber = choosecave()
-	checkCave(caveNumber)
-    
+
+	caveNumber = choosecave() #change to "choosecave()" for simplicity
+	checkCave(caveNumber)#delete this line for simplicity and clean up the logic
+	
 	print('Do you want to play again? (yes or no)')
 	playAgain = input()
 	if playAgain == "no":
